478cc513c4216a7539b6f1b92d722aa62340180c
chhotu1 <chhotukumarsow@gmail.com>
2023-04-26 19:54:10 +0530

    add tab

--- src/Pages/Dashboard/index.js
+++ src/Pages/Dashboard/index.js
@@ -2,22 +2,25 @@ import React from 'react'
 
 const Dashboard = () => {
   return (
-    <div className="p-4 sm:ml-64">
-      <div className="p-4 border-2 border-gray-200 rounded-lg dark:border-gray-700">
-        <ul className="hidden text-sm font-medium text-center text-gray-500 divide-x divide-gray-200 rounded-lg shadow sm:flex dark:divide-gray-700 dark:text-gray-400">
-          <li className="w-full">
-            <a href="#" className="inline-block w-full p-4 text-gray-900 bg-gray-100 rounded-l-lg focus:ring-4 focus:ring-blue-300 active focus:outline-none dark:bg-gray-700 dark:text-white" aria-current="page">Profile</a>
+    <div className="p-4 sm:ml-64 dark">
+      <div className="p-4">
+
+        <ul className="flex flex-wrap text-sm font-medium text-center  dark:text-gray-100 bg-gray-100">
+          <li className="mr-2">
+            <a href="#" aria-current="page" className="inline-block p-2 text-gray-600 white  dark:bg-gray-800 dark:text-blue-500  active shadow">Profile</a>
+          </li> 
+          <li className="mr-2">
+            <a href="#" className="inline-block p-2 hover:text-gray-600 hover:bg-gray-50 dark:hover:bg-gray-800 dark:hover:text-gray-300">Dashboard</a>
           </li>
-          <li className="w-full">
-            <a href="#" className="inline-block w-full p-4 bg-white hover:text-gray-700 hover:bg-gray-50 focus:ring-4 focus:ring-blue-300 focus:outline-none dark:hover:text-white dark:bg-gray-800 dark:hover:bg-gray-700">Dashboard</a>
+          <li className="mr-2">
+            <a href="#" className="inline-block p-2 hover:text-gray-600 hover:bg-gray-50 dark:hover:bg-gray-800 dark:hover:text-gray-300">Settings</a>
           </li>
-          <li className="w-full">
-            <a href="#" className="inline-block w-full p-4 bg-white hover:text-gray-700 hover:bg-gray-50 focus:ring-4 focus:ring-blue-300 focus:outline-none dark:hover:text-white dark:bg-gray-800 dark:hover:bg-gray-700">Settings</a>
+          <li className="mr-2">
+            <a href="#" className="inline-block p-2 hover:text-gray-600 hover:bg-gray-50 dark:hover:bg-gray-800 dark:hover:text-gray-300">Contacts</a>
           </li>
-          <li className="w-full">
-            <a href="#" className="inline-block w-full p-4 bg-white rounded-r-lg hover:text-gray-700 hover:bg-gray-50 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:hover:text-white dark:bg-gray-800 dark:hover:bg-gray-700">Invoice</a>
-          </li>
+          
         </ul>
+
       </div>
     </div>
   )
